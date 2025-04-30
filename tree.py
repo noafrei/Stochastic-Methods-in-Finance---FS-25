efb8d373555f74b90b821ece740767e419bff275
Lilin <vxld100@tuta.io>
2025-04-30 17:51:42 +0200

    Code for points 1-3.

--- tree.py
+++ tree.py
@@ -1,9 +1,48 @@
-import pandas as pd
+from typing import List
 import numpy as np
+from time import time
 
-data = pd.read_csv("./data/Microsoft_2015-2025.csv")
-data = data.rename(columns={'Close/Last': 'Close'})
-data['log_returns'] = np.log(data['Close']).diff()
-std = data['log_returns'].std() * np.sqrt(252)
 
-print(std)
+def add_children(node: List, up : float, down : float, max_depth : int):
+    # layer of the tree is as follows:
+    # [depth, [prices], [avg prices]]
+    depth = node[0]
+    if depth == max_depth:
+        return
+
+    prices = node[1]
+    avg_prices = node[2]
+
+    up_prices = prices * up
+    down_prices = prices * down
+
+    price_sums = depth * avg_prices
+
+    up_averages = (price_sums + up_prices) / (depth + 1)
+    down_averages = (price_sums + down_prices) / (depth + 1)
+
+    node[0] += 1
+    node[1] = np.concatenate((up_prices, down_prices))
+    node[2] = np.concatenate((up_averages, down_averages))
+    add_children(node, up, down, max_depth)
+
+
+tree = [0, np.array([100]), np.array([100])]
+std = 0.2713948019202162
+up_factor = np.exp(std * np.sqrt(1/25))
+down_factor = np.exp(-std * np.sqrt(1/25))
+max_depth = 25
+
+strike_price = 120
+
+start = time()
+add_children(tree, up_factor, down_factor, max_depth)
+end = time()
+print(f"Success! Took {round(end - start, 3)} seconds")
+
+print(tree)
+
+payoffs = tree[2] - strike_price
+payoffs = payoffs.clip(0)
+
+print(payoffs)
