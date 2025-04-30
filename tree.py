81ef3bab6426097e8daa33879c1b86a23ee11c8e
Lilin <vxld100@tuta.io>
2025-04-30 16:37:40 +0200

    Cleaned up dev environment.

---
+++ tree.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+
+data = pd.read_csv("./data/Microsoft_2015-2025.csv")
+data = data.rename(columns={'Close/Last': 'Close'})
+data['log_returns'] = np.log(data['Close']).diff()
+std = data['log_returns'].std() * np.sqrt(252)
+
+print(std)
